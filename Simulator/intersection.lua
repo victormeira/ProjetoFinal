f11ad074499fd8c289f1ae8ec79c22415c858f7d
Victor Meira <victor.meira@bigdatacorp.com.br>
2019-09-17 10:28:44 -0300

    Refactoring intersection to become the whole crossing

--- Simulator/intersection.lua
+++ Simulator/intersection.lua
@@ -1,28 +1,58 @@
 local class = require("middleclass")
+require("vehicle")
 Intersection = class('intersection')
 
-function Intersection:initialize (posX, posY, id)
-    self.posX = posX
-    self.posY = posY
-    self.color = "red"
-    self.nextColor = "green"
+function Intersection:initialize (middlePosX, middlePosY, surroundingBlockSize, flowXDirection, flowYDirection, id)
+
+    self.middlePosXGrid, self.middlePosYGrid = getGridIndex(middlePosX, middlePosY)
     self.nextChange = -1
     self.id = id
-    self.radius = 10
+    self.lightDrawingRadius = 10
+
+    -- creates a "box" of the surrouding crosswalks
+    self.Crosswalks = {}
+    self.Crosswalks.xAxis = {}
+    self.Crosswalks.xAxis.sameAxisPos =  self.middlePosXGrid - (flowXDirection * surroundingBlockSize/2) -- if 1, crossing is on the left
+    self.Crosswalks.xAxis.lowerLimit = self.middlePosYGrid - surroundingBlockSize/2
+    self.Crosswalks.xAxis.upperLimit = self.middlePosYGrid + surroundingBlockSize/2
+    self.Crosswalks.xAxis.lightColor = "green"
+    self.Crosswalks.xAxis.drawPositionX = (self.Crosswalks.xAxis.sameAxisPos - 1) * 10 
+    self.Crosswalks.xAxis.drawPositionY = (self.Crosswalks.xAxis.upperLimit - 1) * 10
+
+
+    self.Crosswalks.yAxis = {}
+    self.Crosswalks.yAxis.sameAxisPos =  self.middlePosYGrid - (flowYDirection * surroundingBlockSize/2) -- if 1, crossing is on the left
+    self.Crosswalks.yAxis.lowerLimit = self.middlePosXGrid - surroundingBlockSize/2
+    self.Crosswalks.yAxis.upperLimit = self.middlePosXGrid + surroundingBlockSize/2
+    self.Crosswalks.yAxis.lightColor = "red"
+    self.Crosswalks.yAxis.drawPositionY = (self.Crosswalks.yAxis.sameAxisPos - 1) * 10 
+    self.Crosswalks.yAxis.drawPositionX = (self.Crosswalks.yAxis.upperLimit - 1) * 10
+
 end
 
 function Intersection:draw()
-    if(self.color == "red") then
-        color = {193/255, 66/255, 66/255, 1}
-    elseif (self.color == "yellow") then
-        color = {215/255, 215/255, 60/255, 1}       
-    else
-        color = {63/255, 191/255, 63/255, 1}
+
+    for k,v in pairs(self.Crosswalks) do 
+        if(v.lightColor == "red") then
+            color = {193/255, 66/255, 66/255, 1}
+        elseif (v.lightColor == "yellow") then
+            color = {215/255, 215/255, 60/255, 1}       
+        else
+            color = {63/255, 191/255, 63/255, 1}
+        end
+        love.graphics.setColor(color)
+        love.graphics.circle("fill", v.drawPositionX, v.drawPositionY, self.lightDrawingRadius)
     end
-    love.graphics.setColor(color)
-    love.graphics.circle("fill", self.posX, self.posY, self.radius)
+
     love.graphics.setColor(1,1,1)
 end
+
+-- TODO: 
+--      Add color change ordering                           -> adding extra delay between changes (if one is green, turn to yellow and keep other; 
+--                                                                                                  if yellow, turn to red and other to green)
+--      Fix is mouseAbove function                          -> should iterate through crosswalks looking at drawing positions
+--      Add light closing "trueing" the crosswalk blocks    -> go through upper and lower limit at sameAxisPos and true those blocks
+
 
 function Intersection:orderColorChange()
     currentColor = self.color
